4b7f238335d6109b600c8543c545acbc77f49681
IagoGOOL <iago.gool@gmail.com>
2024-10-01 11:26:07 -0300

    Criação do projeto de DAC

---
+++ src/main/Java/com/entities/Aluno.java
@@ -0,0 +1,78 @@
+package com.atividadejpa.entities;
+
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+@Table(name="tb_alunos")
+public class Aluno implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotNull
+    private String nome;
+    @NotNull
+    private String matricula;
+    @NotNull String email;
+    
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Orientador orientador;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Orientador getOrientador() {
+        return orientador;
+    }
+
+    public void setOrientador(Orientador orientador) {
+        this.orientador = orientador;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "AlunoEntity{" + "id=" + id + ", nome=" + nome + ", matricula=" + matricula + ", email=" + email + '}';
+    }
+    
+}
